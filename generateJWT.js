40daeeeb7fb50398fb99897590eb6c4f250a6b42
radik.k <radik.k@magenable.com.au>
2025-10-09 14:36:28 +0600

    #6472 jwt exp

--- generateJWT.js
+++ generateJWT.js
@@ -2,6 +2,6 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 const JWTSecretKey = process.env.JWT_SECRET_KEY;
-const token = jwt.sign({ username: "MCP User" }, JWTSecretKey, { expiresIn: '4h' });
+const token = jwt.sign({ username: "MCP User" }, JWTSecretKey, { expiresIn: '1Y' });
 console.log('Use this header for auth in your MCP client:\n')
 console.log(`Authorization: Bearer ${token}\n`)
